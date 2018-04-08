501140051a6abe5c088e8aa0aeee4869a294b252
Kyohei Tsuda <shimano.entou@gmail.com>
2018-04-08 19:33:57 +0900

    複数ファイルに対応

--- head.go
+++ head.go
@@ -11,30 +11,50 @@ var (
 	nOpt = flag.Int("n", 10, "help message for n option")
 )
 
-func main() {
+func get_args() (int, []string) {
 	flag.Parse()
-	file_path := flag.Args()
-
-	if len(file_path) < 1 {
+	args := flag.Args()
+	if len(args) < 1 {
 		fmt.Println("Invalid argument, you must specify a path.")
 		os.Exit(1)
 	}
 
-	file, err := os.Open(file_path[0])
-	if err != nil {
-		fmt.Println("Failed to open file.")
-		os.Exit(2)
-	}
-	defer file.Close()
+	return len(args), args
+}
 
-	fmt.Println(*nOpt)
+func print_rows(file *os.File) {
 	sc := bufio.NewScanner(file)
-
 	for i := 1; sc.Scan(); i++ {
 		if err := sc.Err(); err != nil {
 			os.Exit(1)
 			break
 		}
 		fmt.Printf("%s\n", sc.Text())
+		if i == *nOpt {
+			break
+		}
+	}
+}
+
+func main() {
+	len, args := get_args()
+	for i, path := range args {
+		file, err := os.Open(path)
+
+		if i >= 1 {
+			fmt.Printf("\n")
+		}
+
+		if len != 1 {
+			fmt.Printf("==> %s <==\n", path)
+		}
+
+		if err != nil {
+			fmt.Println("Failed to open file.")
+			os.Exit(2)
+		}
+		defer file.Close()
+
+		print_rows(file)
 	}
 }
